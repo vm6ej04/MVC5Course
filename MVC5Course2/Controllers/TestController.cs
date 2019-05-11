79c1f865f795353052c481a6c23d22b7d6710fd6
kevin <kevin@champtron.com>
2019-05-11 12:06:18 +0800

    GoRedirect 實作，應用在test/Redir上

---
+++ MVC5Course2/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course2.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Redir()
+        {
+            ViewBag.Msg = "Message";
+            ViewBag.Url = "/Home/About";
+            return View("GoRedirect");
+        }
+    }
+}
